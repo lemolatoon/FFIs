1f95ed5e8a2537bf4b127891aa17992d98c77832
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-08-19 04:03:12 +0900

    Fix rsp 16 bytes alignment before call fortran function

--- src/fortran.f90
+++ src/fortran.f90
@@ -2,11 +2,11 @@ subroutine hello_fortran() bind(c)
 ! subroutine main() bind(c)
     implicit none
     real pi
-    interface
-        subroutine print_rsp() bind(c)
-        end subroutine
-    end interface
+    ! interface
+    !     subroutine print_rsp() bind(c)
+    !     end subroutine
+    ! end interface
     print *, "Hello from Fortran"
     pi = atan(1.0) * 4.0
-    print *, "pi = ", 1.0 
+    print *, "pi = ", pi 
 end subroutine
